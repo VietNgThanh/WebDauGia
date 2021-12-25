fec175f7d4bdd31c47eb383da65d1299e3f44705
ntdkhoaaaa <96461871+ntdkhoaaaa@users.noreply.github.com>
2021-12-25 11:59:09 +0700

    db moi nhat

--- web_dau_gia .sql
+++ web_dau_gia .sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 24, 2021 at 03:27 AM
+-- Generation Time: Dec 25, 2021 at 05:46 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 7.4.22
 
@@ -128,68 +128,67 @@ CREATE TABLE `orders_product` (
   `id_Product` int(11) DEFAULT NULL,
   `id_User` int(11) DEFAULT NULL,
   `Price` int(11) DEFAULT NULL,
-  `Time_make_price` datetime DEFAULT NULL,
-  `rule` int(11) DEFAULT NULL
+  `Time_make_price` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 --
 -- Dumping data for table `orders_product`
 --
 
-INSERT INTO `orders_product` (`idOrder`, `id_Product`, `id_User`, `Price`, `Time_make_price`, `rule`) VALUES
-(1, 1, 2, 12000000, '2021-12-12 00:00:00', 1),
-(2, 1, 5, 12222222, '2021-12-18 22:21:26', 0),
-(3, 1, 6, 12200000, '2021-12-18 23:22:52', 0),
-(4, 1, 7, 12055000, '2021-12-18 23:23:29', 0),
-(5, 2, 8, 120, '2021-12-18 22:25:03', 1),
-(6, 2, 6, 125, '2021-12-18 22:25:33', 0),
-(7, 2, 7, 126, '2021-12-18 22:25:54', 0),
-(8, 3, 2, 111, '2021-12-18 22:26:10', 1),
-(9, 4, 2, 1581919, '2021-12-21 17:05:10', 1),
-(10, 5, 2, 6119929, '2021-12-21 17:05:35', 1),
-(11, 4, 5, 1681919, '2021-12-21 17:06:12', 0),
-(12, 5, 5, 6219929, '2021-12-21 17:06:40', 0),
-(13, 5, 5, 6319929, '2021-12-22 12:21:58', 0),
-(14, 1, 6, 14000000, '2021-12-22 12:59:59', 0),
-(15, 1, 5, 15000000, '2021-12-22 13:02:52', 0),
-(16, 1, 6, 16000000, '2021-12-22 13:04:31', 0),
-(17, 1, 5, 17000000, '2021-12-22 14:06:00', 0),
-(18, 1, 6, 20000000, '2021-12-22 14:07:14', 0),
-(20, 1, 5, 22000000, '2021-12-22 16:40:40', 0),
-(22, 1, 6, 21000000, '2021-12-22 16:44:01', 0),
-(23, 1, 5, 23000000, '2021-12-22 16:52:03', 0),
-(24, 1, 5, 23000000, '2021-12-22 16:53:10', 0),
-(25, 1, 6, 24000000, '2021-12-22 16:58:09', 0),
-(26, 1, 5, 24000000, '2021-12-22 17:01:42', 0),
-(27, 1, 6, 24000000, '2021-12-22 17:03:29', 0),
-(28, 2, 5, 13000000, '2021-12-22 17:05:16', 0),
-(30, 3, 5, 19000000, '2021-12-22 17:13:23', 0),
-(31, 3, 6, 20000000, '2021-12-22 17:17:56', NULL),
-(32, 3, 5, 19500000, '2021-12-22 17:18:41', 0),
-(33, 3, 6, 19500000, '2021-12-22 17:19:14', 0),
-(34, 3, 5, 19500000, '2021-12-22 17:22:04', NULL),
-(35, 3, 5, 22000000, '2021-12-22 17:23:57', 0),
-(36, 3, 6, 23000000, '2021-12-22 17:24:54', NULL),
-(37, 3, 5, 22000000, '2021-12-22 17:25:22', NULL),
-(38, 3, 5, 23000000, '2021-12-22 17:26:04', NULL),
-(39, 3, 6, 24000000, NULL, NULL),
-(40, 4, 5, 13000000, '2021-12-22 13:14:48', NULL),
-(41, 4, 5, 13000000, '2021-12-22 13:14:48', NULL),
-(42, 3, 5, 25000000, NULL, 0),
-(43, 3, 6, 24500000, NULL, NULL),
-(44, 3, 6, 25000000, NULL, NULL),
-(46, 7, 2, 13000000, '2021-12-23 06:22:57', 1),
-(47, 7, 2, 13000000, '2021-12-23 06:22:57', 1),
-(48, 9, 2, 13000000, '2021-12-23 06:55:56', NULL),
-(49, 9, 2, 13000000, '2021-12-23 06:55:56', 1),
-(50, 9, 2, 13000000, '2021-12-22 17:26:04', 1),
-(51, 7, 5, 16000000, NULL, 0),
-(52, 7, 6, 15000000, NULL, 0),
-(53, 7, 6, 15800000, NULL, 0),
-(54, 7, 7, 16500000, NULL, NULL),
-(55, 7, 5, 16500000, NULL, 0),
-(56, 7, 6, 19000000, NULL, NULL),
-(57, 7, 7, 19000000, NULL, NULL);
+INSERT INTO `orders_product` (`idOrder`, `id_Product`, `id_User`, `Price`, `Time_make_price`) VALUES
+(1, 1, 2, 12000000, '2021-12-12 00:00:00'),
+(2, 1, 5, 12222222, '2021-12-18 22:21:26'),
+(3, 1, 6, 12200000, '2021-12-18 23:22:52'),
+(4, 1, 7, 12055000, '2021-12-18 23:23:29'),
+(5, 2, 8, 120, '2021-12-18 22:25:03'),
+(6, 2, 6, 125, '2021-12-18 22:25:33'),
+(7, 2, 7, 126, '2021-12-18 22:25:54'),
+(8, 3, 2, 111, '2021-12-18 22:26:10'),
+(9, 4, 2, 1581919, '2021-12-21 17:05:10'),
+(10, 5, 2, 6119929, '2021-12-21 17:05:35'),
+(11, 4, 5, 1681919, '2021-12-21 17:06:12'),
+(12, 5, 5, 6219929, '2021-12-21 17:06:40'),
+(13, 5, 5, 6319929, '2021-12-22 12:21:58'),
+(14, 1, 6, 14000000, '2021-12-22 12:59:59'),
+(15, 1, 5, 15000000, '2021-12-22 13:02:52'),
+(16, 1, 6, 16000000, '2021-12-22 13:04:31'),
+(17, 1, 5, 17000000, '2021-12-22 14:06:00'),
+(18, 1, 6, 20000000, '2021-12-22 14:07:14'),
+(20, 1, 5, 22000000, '2021-12-22 16:40:40'),
+(22, 1, 6, 21000000, '2021-12-22 16:44:01'),
+(23, 1, 5, 23000000, '2021-12-22 16:52:03'),
+(24, 1, 5, 23000000, '2021-12-22 16:53:10'),
+(25, 1, 6, 24000000, '2021-12-22 16:58:09'),
+(26, 1, 5, 24000000, '2021-12-22 17:01:42'),
+(27, 1, 6, 24000000, '2021-12-22 17:03:29'),
+(28, 2, 5, 13000000, '2021-12-22 17:05:16'),
+(30, 3, 5, 19000000, '2021-12-22 17:13:23'),
+(31, 3, 6, 20000000, '2021-12-22 17:17:56'),
+(32, 3, 5, 19500000, '2021-12-22 17:18:41'),
+(33, 3, 6, 19500000, '2021-12-22 17:19:14'),
+(34, 3, 5, 19500000, '2021-12-22 17:22:04'),
+(35, 3, 5, 22000000, '2021-12-22 17:23:57'),
+(36, 3, 6, 23000000, '2021-12-22 17:24:54'),
+(37, 3, 5, 22000000, '2021-12-22 17:25:22'),
+(38, 3, 5, 23000000, '2021-12-22 17:26:04'),
+(39, 3, 6, 24000000, NULL),
+(40, 4, 5, 13000000, '2021-12-22 13:14:48'),
+(41, 4, 5, 13000000, '2021-12-22 13:14:48'),
+(42, 3, 5, 25000000, NULL),
+(43, 3, 6, 24500000, NULL),
+(44, 3, 6, 25000000, NULL),
+(46, 7, 2, 13000000, '2021-12-23 06:22:57'),
+(47, 7, 2, 13000000, '2021-12-23 06:22:57'),
+(48, 9, 2, 13000000, '2021-12-23 06:55:56'),
+(49, 9, 2, 13000000, '2021-12-23 06:55:56'),
+(50, 9, 2, 13000000, '2021-12-22 17:26:04'),
+(51, 7, 5, 16000000, NULL),
+(52, 7, 6, 15000000, NULL),
+(53, 7, 6, 15800000, NULL),
+(54, 7, 7, 16500000, NULL),
+(55, 7, 5, 16500000, NULL),
+(56, 7, 6, 19000000, NULL),
+(57, 7, 7, 19000000, NULL);
 
 --
 -- Triggers `orders_product`
@@ -253,7 +252,7 @@ CREATE TABLE `product` (
   `id_Cat` int(11) DEFAULT NULL,
   `User_id` int(11) DEFAULT NULL,
   `Detail_tiny` varchar(45) DEFAULT NULL,
-  `Detail_full` varchar(45) DEFAULT NULL,
+  `Detail_full` text DEFAULT NULL,
   `Start_price` int(11) DEFAULT NULL,
   `Imme_Price` double DEFAULT NULL,
   `Availability` int(11) DEFAULT NULL,
@@ -262,23 +261,25 @@ CREATE TABLE `product` (
   `id_Bidder_current` int(11) DEFAULT NULL,
   `highest_price` int(11) NOT NULL,
   `buoc_nhay` int(11) DEFAULT NULL,
-  `dathongbao` int(11) DEFAULT NULL
+  `dathongbao` int(11) DEFAULT NULL,
+  `time_to_close` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`idProduct`, `Name`, `id_Cat`, `User_id`, `Detail_tiny`, `Detail_full`, `Start_price`, `Imme_Price`, `Availability`, `Current_Price`, `id_ParentCat`, `id_Bidder_current`, `highest_price`, `buoc_nhay`, `dathongbao`) VALUES
-(1, 'iphone7', 2, 3, 'abc', 'abcvf', 10000000, 100000000, 1, 24000000, 1, 6, 24000000, 100000, NULL),
-(2, 'iphone X', 2, 3, 'abc', 'abcvferr', 12000000, 100000000, 1, 100126, 1, 5, 0, 100000, NULL),
-(3, 'iphone 13', 2, 3, 'abc', 'abcvferr', 18000000, 100000000, 1, 25000000, 1, 5, 25000000, 100000, NULL),
-(4, 'iPad', 3, 8, 'abc', 'abcvferr', 12000000, 100000000, 1, 13000000, 1, 5, 13000000, 100000, NULL),
-(5, 'iPad Pro', 3, 8, 'abc', 'abcvferr', 15000000, 100000000, 1, 15000000, 1, NULL, 0, 100000, NULL),
-(6, 'Túi Gucci', 5, 3, 'abc', 'abcvferr', 15000000, 100000000, 1, 15000000, 2, NULL, 0, 100000, NULL),
-(7, 'Túi Dior', 5, 3, 'abc', 'abcvferr', 15000000, 100000000, 1, 19000000, 2, 6, 19000000, 100000, NULL),
-(8, 'iphone8', 2, 3, 'abc', 'abcvjhwbjhwbviweb', 12000000, 100000000, 1, 12000000, 1, NULL, 0, 100000, NULL),
-(9, 'Play Station', 13, 3, 'abc', 'ebubebhbchjbjwbieb', 10000000, 100000000, 1, 13000000, 4, 2, 13000000, 100000, NULL);
+INSERT INTO `product` (`idProduct`, `Name`, `id_Cat`, `User_id`, `Detail_tiny`, `Detail_full`, `Start_price`, `Imme_Price`, `Availability`, `Current_Price`, `id_ParentCat`, `id_Bidder_current`, `highest_price`, `buoc_nhay`, `dathongbao`, `time_to_close`) VALUES
+(0, 'Túi Dior 2', 5, 3, 'abc', 'fbuwbfibisbisbisbvisb', 12000000, 100000000, 1, 12000000, 2, NULL, 0, 100000, NULL, '2021-12-31 17:37:50'),
+(1, 'iphone7', 2, 3, 'abc', 'abcvf', 10000000, 100000000, 1, 24000000, 1, 6, 24000000, 100000, NULL, NULL),
+(2, 'iphone X', 2, 3, 'abc', 'abcvferr', 12000000, 100000000, 1, 100126, 1, 5, 0, 100000, NULL, NULL),
+(3, 'iphone 13', 2, 3, 'abc', 'abcvferr', 18000000, 100000000, 1, 25000000, 1, 5, 25000000, 100000, NULL, NULL),
+(4, 'iPad', 3, 8, 'abc', 'abcvferr', 12000000, 100000000, 1, 13000000, 1, 5, 13000000, 100000, NULL, NULL),
+(5, 'iPad Pro', 3, 8, 'abc', 'abcvferr', 15000000, 100000000, 1, 15000000, 1, NULL, 0, 100000, NULL, NULL),
+(6, 'Túi Gucci', 5, 3, 'abc', 'abcvferr', 15000000, 100000000, 1, 15000000, 2, NULL, 0, 100000, NULL, NULL),
+(7, 'Túi Dior', 5, 3, 'abc', 'abcvferr', 15000000, 100000000, 1, 19000000, 2, 6, 19000000, 100000, NULL, NULL),
+(8, 'iphone8', 2, 3, 'abc', 'abcvjhwbjhwbviweb', 12000000, 100000000, 1, 12000000, 1, NULL, 0, 100000, NULL, NULL),
+(9, 'Play Station', 13, 3, 'abc', 'ebubebhbchjbjwbieb', 10000000, 100000000, 1, 13000000, 4, 2, 13000000, 100000, NULL, NULL);
 
 --
 -- Triggers `product`
@@ -288,6 +289,9 @@ CREATE TRIGGER `product_BEFORE_INSERT` BEFORE INSERT ON `product` FOR EACH ROW B
 	set @pacat=0;
 	select parent_id into @pacat from childcategory where id=new.id_Cat;
     set new.id_ParentCat=@pacat;
+    set @time_to_close=0;
+    select addtime(now(),'7 00:00:00') into @time_to_close;
+    set new.time_to_close=@time_to_close;
 END
 $$
 DELIMITER ;
@@ -301,7 +305,7 @@ DELIMITER ;
 CREATE TABLE `users` (
   `iduser` int(11) NOT NULL,
   `username` varchar(45) DEFAULT NULL,
-  `password` varchar(45) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
   `name` varchar(60) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
   `address` varchar(45) DEFAULT NULL,
@@ -342,6 +346,11 @@ CREATE TABLE `wish_list` (
 --
 -- Indexes for table `childcategory`
 --
+
+
+
+
+
 ALTER TABLE `childcategory`
   ADD PRIMARY KEY (`id`) USING BTREE,
   ADD KEY `FK_parent_id` (`parent_id`) USING BTREE;
@@ -371,14 +380,20 @@ ALTER TABLE `parentcategory`
 --
 ALTER TABLE `product`
   ADD PRIMARY KEY (`idProduct`) USING BTREE,
+
   ADD KEY `fk_Cata_Pro` (`id_Cat`) USING BTREE,
   ADD KEY `fk_User_Product` (`User_id`) USING BTREE;
+
+ALTER TABLE `product`
+  MODIFY `idProduct` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`iduser`) USING BTREE;
+ALTER TABLE `users`
+  MODIFY `iduser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Indexes for table `wish_list`
@@ -437,19 +452,13 @@ ALTER TABLE `orders_product`
   ADD CONSTRAINT `fk_Order_Pro` FOREIGN KEY (`id_Product`) REFERENCES `product` (`idProduct`),
   ADD CONSTRAINT `fk_Order_User` FOREIGN KEY (`id_User`) REFERENCES `users` (`iduser`);
 
--- Add full-text search for table product
-ALTER TABLE product
-    ADD FULLTEXT (Name);
-
--- Add full-text search for table childcategory
-ALTER TABLE childcategory
-    ADD FULLTEXT (name);
-
 --
 -- Constraints for table `product`
 --
 ALTER TABLE `product`
   ADD CONSTRAINT `fk_User_Product` FOREIGN KEY (`User_id`) REFERENCES `users` (`iduser`);
+
+
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
